44432da08a5a7d158760d91e94a1bfaa8f13f914
mehta.anshuman@gmail.com <mehta.anshuman@gmail.com@2131ece8-f408-11de-8f43-4547254af6c6>
2009-12-29 03:26:28 +0000

    - bat files - clips via dos if superfast

--- trunk/clipsHousing/make.bat
+++ trunk/clipsHousing/make.bat
@@ -1,13 +1,10 @@
 @ECHO off
-DIR  . 
 
 cd protege
 
-TYPE  AIProject2pprj.pins > ../ai2KBS.clp
+TYPE  AIProject2pprj.pont > ../ai2KBS.clp
 echo (definstances ai2KBSinstances>> ../ai2KBS.clp
-TYPE  AIProject2pprj.pont >> ../ai2KBS.clp
+TYPE  AIProject2pprj.pins >> ../ai2KBS.clp
 echo ) >> ../ai2KBS.clp
 
 cd ..
-
-PAUSE
